ba77e438c1765533f64bf8531da5636e8aa2375d
mlk <sungh7330@naver.com>
2023-05-10 15:33:13 +0900

    Initial commit

---
+++ src/DT.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class DT {
+   
+
+    public static void main(String[] args) {
+       Student S = new Student();
+       S.Setname("무무");
+       String a = S.Getname();
+        System.out.println("a = " + a);
+
+
+    }우와!
+    }
+
---
+++ src/Student.java
@@ -0,0 +1,12 @@
+public class Student {
+    private String name;
+    private String id;
+
+
+    public String Getname(){
+        return name;
+    }
+    public void Setname(String name){
+        this.name=name;
+    }
+}
